d80f0ae352c8146b6a59cec4850c4e7b27d7fd01
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2021-07-23 23:46:16 +0200

    fix image loading when images are cached

--- src/helpers/preloadImage.ts
+++ src/helpers/preloadImage.ts
@@ -1,9 +1,9 @@
 export function preloadImage(src: string): Promise<void> {
   return new Promise((resolve, reject) => {
     const loadImg = new Image();
-    loadImg.src = src;
     loadImg.onload = () => resolve();
     loadImg.onerror = reject;
+    loadImg.src = src;
   });
 }
 
