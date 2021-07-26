c5f3cd4b69b5de1c3a6cc522cb9c54ecc84ca7a7
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2021-07-26 18:45:14 +0200

    remove monorepo

--- packages/api/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const server = app.listen(app.get('port'), () => {
-  console.log(`App is running at ${app.get('port')}`);
-  console.log('Press Ctrl-C to stop it\n');
-});
-
-export default server;
