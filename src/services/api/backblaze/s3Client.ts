d37be7fd4fdcb7cec4619c7a9aa9def574b0f7b7
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2021-08-16 21:31:57 +0200

    rename AWS env variables

--- src/services/api/backblaze/s3Client.ts
+++ src/services/api/backblaze/s3Client.ts
@@ -1,10 +1,22 @@
 import AWS from 'aws-sdk';
 
 function getS3Client(): AWS.S3 {
-  const { B2_ENDPOINT } = process.env;
-  if (!B2_ENDPOINT) {
+  const {
+    API_AWS_ACCESS_KEY_ID,
+    API_AWS_SECRET_ACCESS_KEY,
+    B2_ENDPOINT
+  } = process.env;
+
+  if (!B2_ENDPOINT || !API_AWS_ACCESS_KEY_ID || !API_AWS_SECRET_ACCESS_KEY) {
     throw new Error('bad b2 environment');
   }
+
+  const credentials = new AWS.Credentials({
+    accessKeyId: API_AWS_ACCESS_KEY_ID,
+    secretAccessKey: API_AWS_SECRET_ACCESS_KEY,
+  });
+
+  AWS.config.credentials = credentials;
 
   const region =  B2_ENDPOINT.split('.')[1];
   AWS.config.signatureVersion = 'v4';
